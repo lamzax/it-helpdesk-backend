8d81d4e7def7e6ded92aa189c984ba1e1172394e
lamzax <gliepin2@gmail.com>
2026-08-26 08:22:19 +0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -15,6 +15,8 @@ const accessRightsRouter = require('./routes/accessRights');
 const importRouter = require('./routes/importData');
 const uploadsRouter = require('./routes/uploads');
 const customFieldsRouter = require('./routes/customFields');
+const assetCategoriesTreeRouter = require('./routes/assetCategoriesTree');
+const exportCsvRouter = require('./routes/exportCsv');
 
 const app = express();
 app.use(helmet({ contentSecurityPolicy: false })); // atslegts CSP, lai admin panelis var ielādēt CDN skriptus (PapaParse)
@@ -34,6 +36,8 @@ app.use('/api/access-rights', accessRightsRouter);
 app.use('/api/import', importRouter);
 app.use('/api/uploads', uploadsRouter);
 app.use('/api/custom-fields', customFieldsRouter);
+app.use('/api/asset-categories', assetCategoriesTreeRouter);
+app.use('/api/export', exportCsvRouter);
 
 // Augšupielādētie pielikumi (foto/video/balss ziņas) -- pieejami statiski
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
